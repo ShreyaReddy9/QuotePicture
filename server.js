a42c76febd066ff2a9b0318870b75f8695a24db3
ShreyaReddy9 <gudurushreya.16@gmail.com>
2025-04-30 11:44:31 -0700

    Add all recent changes

--- server.js
+++ server.js
@@ -31,4 +31,5 @@ app.get('/images', (req, res) => {
   });
 });
 
-app.listen(5000, () => console.log('Server running on port 5000'));
+app.listen(5000, () => console.log(`Server running at http://localhost:5000/`));
+
